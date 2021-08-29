3884a62886309be1763be943d78f9866012f9f3a
CitlalliChavez <zaydacit@hotmail.com>
2021-08-29 03:25:38 -0500

    users-bcryptjs

---
+++ app/models/usuarios.models.js
@@ -0,0 +1,104 @@
+
+const { Usuarios } = require("../../db");
+const sequelize = require('../../db/conexion');
+const bcryptjs = require('bcryptjs');
+
+class UsuarioModels {
+
+    async getUsuarios() {
+        const usuario = await Usuarios.findAll();
+        if(!usuario ) {
+            return {
+                error: true,
+                msg: 'No se logró encontrar ningun usuario',
+                status: 400
+            };
+        }
+        return usuario;
+    };
+//buscar usuario por ID
+    async getUsuarioId({ id }) {
+        const usuario = await Usuarios.findByPk(id);
+        if(!usuario ) {
+            return {
+                error: true,
+                msg: 'No se logró encontrar ningun usuario',
+                status: 400
+            };
+        }
+        return usuario; 
+    };
+//existencia de usuario por EMAIL
+    async getUsuarioEmail({ email }) {
+        const usuario = await Usuarios.findOne({
+            where: { email }
+        });
+        console.log(usuario);
+        if(!usuario) {
+            return {
+                error: true,
+                msg: 'Email o password son incorrectos',
+                status: 400
+            };
+        }
+        return usuario;
+    };
+
+    async newUsuario ({nombres, apellidos,usuario, email, pass}) {
+        console.log (email,nombres, apellidos)
+        const usuarioExiste = await this.getUsuarioEmail({email: email});
+        console.log(usuarioExiste);
+        console.log(!usuarioExiste.error);
+        if(!usuarioExiste.error) {
+            return {
+                error: true,
+                msg: 'El usuario con ese E-mail ya existe',
+                status: 400
+            };
+        };
+        const usuarios = await Usuarios.create({ 
+            nombres, 
+            apellidos, 
+            usuario,
+            email, 
+            pass
+        });
+        const salt = bcryptjs.genSaltSync(10);
+        usuarios.pass = bcryptjs.hashSync(pass, salt);
+        await usuarios.save();
+        delete usuarios.dataValues.pass;
+        return usuarios;
+    };
+
+    async putUsuario({ id, body }) {
+        const { nombres, apellidos, email, pass } = body;
+        const usuario = await Usuarios.findByPk(id);
+        if (!usuario ){
+            return {
+                error: true,
+                msg: 'No se logró encontrar ningun usuario',
+                status: 400
+            };
+        };
+        await usuario.update({ nombres, apellidos, email, pass });
+        return usuario;
+    };
+
+    async deleteUsuario({ id }) {
+        const usuario = await Usuarios.findByPk(id);
+        if (!usuario ){
+            return {
+                error: true,
+                msg: 'No se logró encontrar ningun usuario',
+                status: 400
+            };
+        }
+        await usuario.destroy({ 
+            where: {id_usuarios: id} });
+        return true;
+    }
+
+}
+
+
+module.exports = UsuarioModels;
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const cors = require('cors');
 const sequelize = require('./db/conexion');
 const routePresupuestos = require('./app/views/presupuestos.view');
 const routeUsuarios = require('./app/views/usuarios.view');
+const {  Usuarios } = require ('./db/index')
 
-
+app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
 app.use(cors());
 //app.use(midd.limiter);
@@ -17,6 +18,7 @@ app.set('views', __dirname + '/views')
 
 //middleware para captura de errores globales.
 app.use((err, req, res, next)=> {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     console.log(err);
     if (!err){
         return next();
@@ -27,7 +29,7 @@ app.use((err, req, res, next)=> {
 //Iniciar el Servidor
 async function inicioServidor() {
     try {
-      
+        await Usuarios.sync({alter:true});
         await sequelize.authenticate();
         console.log('Conexion con la DB correcta!');
         app.listen(process.env.PORT, function (){
